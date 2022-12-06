61fc8134f7603bad3fae8e0537c1e842d21890f8
PrajaktaShewale <shewalep94@gmail.com>
2022-12-06 14:36:30 +0530

    [Prajakta]Add:In game Player rolls the die to get number between 1 to 6

---
+++ SnakeANDLadder/DieRoll.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeANDLadder
+{
+    internal class DieRoll
+    {
+        public void dieroll()
+        {
+            Random random = new Random();
+            int die = random.Next(1, 7);
+            Console.WriteLine("You get number "+ die);
+        }
+    }
+}
--- SnakeANDLadder/Program.cs
+++ SnakeANDLadder/Program.cs
@@ -9,13 +9,17 @@ namespace SnakeANDLadder
             while(Flag)
             {
                 Console.WriteLine("Welcome to Snake And Ladder Game");
-                Console.WriteLine("1.start " + "\n" + "2.Exit");
+                Console.WriteLine("1.start " + "\n" + "2.DieRoll" + "\n" + "3.Exit");
                 int check = Convert.ToInt32(Console.ReadLine());
                 switch (check)
                 {
                     case 1:
                         StartGame st = new StartGame();
                         st.Game();
+                        break;
+                        case 2:
+                            DieRoll roll = new DieRoll();
+                        roll.dieroll();
                         break;
                         default:
                         Flag = false;
